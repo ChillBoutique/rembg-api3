6ed74e2c36a70eae4628e613af8495e085f1bbe6
ChillBoutique <info@chillboutiqueconsignment.com>
2025-06-23 22:31:59 -0700

    Add files via upload

--- main.py
+++ main.py
@@ -4,6 +4,10 @@ from fastapi.responses import Response
 
 app = FastAPI()
 
+@app.get("/")
+def root():
+    return {"status": "ok"}
+
 @app.post("/remove-background/")
 async def remove_bg(file: UploadFile = File(...)):
     input_data = await file.read()
